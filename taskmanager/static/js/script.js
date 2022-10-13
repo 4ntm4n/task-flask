fc3b7426ffd5379593915caf8fe107b2804f02b5
Anton A <anton.askling@gmail.com>
2022-10-13 03:48:18 +0200

    prepare for heroku push

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+});
+
+let datePicker = document.querySelectorAll(".datepicker");
+M.Datepicker.init(datePicker, {
+  format: "dd mmmm, yyyy",
+  i18n: { done: "Select" },
+});
+
+let selects = document.querySelectorAll('select');
+M.FormSelect.init(selects);
+
+let collapsibles = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsibles);
